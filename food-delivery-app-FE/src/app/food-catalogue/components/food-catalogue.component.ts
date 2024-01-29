64faf7a42e05f782bb7bf1a2ccb116513df10f88
Thomas Berteau <berteau.thomas@hotmail.fr>
2024-01-29 09:21:03 +0100

    food catalogue FE

---
+++ food-delivery-app-FE/src/app/food-catalogue/components/food-catalogue.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-catalogue',
+  templateUrl: './food-catalogue.component.html',
+  styleUrls: ['./food-catalogue.component.css']
+})
+export class FoodCatalogueComponent {
+
+}
